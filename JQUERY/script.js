4feae25f6af08730aba48ee2a884cddc9c7e6825
Christian Liljenquist <christianliljenquist@Christians-MacBook-Pro.local>
2018-04-02 12:15:09 -0400

    change

---
+++ JQUERY/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".imageone").hide();
+    });
+
+    $("#show").click(function(){
+        $(".imageone").show();
+    });
+
+  
+
+
+
+    $(".screen").hover(function(){
+        $(".screen").fadeOut(1000);
+  });
+
+  $(".imghover").hover(function(){
+      $(".screen").fadeIn(1000);
+});
+
+$(".video").hover(function(){
+    $("a",).css("background-color", "blue");
+    $("a",).css("color", "white");
+});
+
+$("#clickhere").click(function(){
+    $(".video").show();
+});
+
+$("#replacetext").hover(function(){
+$("#replace").html("<p>Repeat the process and enjoy!</p>");
+});
+
+
+
+
+
+});
